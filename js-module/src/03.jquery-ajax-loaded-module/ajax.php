c9cf78fcfb936eb11197716f963c9bd785d2a871
Gabriel Humeniuc <humeniuc@gmail.com>
2025-09-24 22:42:28 +0300

    mai mult log

--- js-module/src/03.jquery-ajax-loaded-module/ajax.php
+++ js-module/src/03.jquery-ajax-loaded-module/ajax.php
@@ -1,7 +1,18 @@
 <?php ob_start(); ?>
 <input type="text" value="0" id="val" />
+
 <button id="plus">+</button>
 <button id="minus">-</button>
+
+<script id="normal_script">
+console.log('normal script loaded through ajax');
+console.log('you should see and a module script loaded');
+</script>
+
+<script id="module_script" type="module">
+console.log('module script');
+</script>
+
 <script>
 console.log('<script> from ajax is executed. calling import(\'modules_root/module_events.js\')');
 import('modules_root/module_events.js').then(function(module) {
@@ -9,6 +20,7 @@ import('modules_root/module_events.js').then(function(module) {
     module.registerEvents();
 });
 </script>
+
 <?php
 $html = ob_get_clean();
 
--- js-module/src/03.jquery-ajax-loaded-module/index.php
+++ js-module/src/03.jquery-ajax-loaded-module/index.php
@@ -1,3 +1,8 @@
+<?php 
+$jqueryUrl = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js';
+// $jqueryUrl = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.js';
+// $jqueryUrl = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js';
+?>
 <html>
     <head>
         <script type="importmap">
@@ -8,18 +13,13 @@
         }
         </script>
 
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js"></script>
+        <script src="<?= $jqueryUrl ?>"></script>
     </head>
     <body>
         <button id="ajaxLauncher">Load Module</button>
         <div id="content"></div>
+
         <script src="index.js"></script>
-
-
-        <script type="module">
-        // import { registerEvents } from 'modules_root/module_events.js';
-        </script>
         
         <?php require __DIR__. '/help.php'; ?>
     </body>
